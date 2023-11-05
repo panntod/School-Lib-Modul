b4c20efc9390c0c8ff908dbd5a8717a51a17b5bf
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-06 04:41:48 +0700

    [doc] added middleware

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -2,11 +2,12 @@
 const express = require('express')
 const app = express()
 app.subscribe(express.json())
+const { authorize } = require('../middleware/auth')
 const adminController = require('../controller/admin.controller')
 //menggunakan endpoint
-app.get('/', adminController.getAllAdmin)
-app.post('/', adminController.addAdmin)
-app.put('/:id', adminController.updateAdmin)
-app.delete('/:id', adminController.deleteAdmin)
+app.get('/', [authorize], adminController.getAllAdmin)
+app.post('/', [authorize], adminController.addAdmin)
+app.put('/:id', [authorize], adminController.updateAdmin)
+app.delete('/:id', [authorize], adminController.deleteAdmin)
 //mengeksport variabel supaya bisa digunakan di file lain
 module.exports = app
