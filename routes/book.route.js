3d5376688fbdfaf3e7048bafacb48959da241903
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-06 04:44:57 +0700

    [doc] added middleware

--- routes/book.route.js
+++ routes/book.route.js
@@ -2,12 +2,13 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+const { authorize } = require('../middleware/auth')
 const bookController = require("../controller/book.controller");
 // menggunakan endpoint
-app.get("/", bookController.getAllBook);
-app.post("/find", bookController.findBook);
-app.post("/", bookController.addBook);
-app.put("/:id", bookController.updateBook);
-app.delete("/:id", bookController.deleteBook);
+app.get("/", [authorize], bookController.getAllBook);
+app.post("/find", [authorize], bookController.findBook);
+app.post("/", [authorize], bookController.addBook);
+app.put("/:id", [authorize], bookController.updateBook);
+app.delete("/:id", [authorize], bookController.deleteBook);
 //mengeksport variabel supaya bisa digunakan di file lain
 module.exports = app;
