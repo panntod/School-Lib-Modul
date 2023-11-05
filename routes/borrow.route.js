f6a9600d288c204ce0d4beace0eab7a8eca228a4
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-06 04:45:08 +0700

    [doc] added middleware

--- routes/borrow.route.js
+++ routes/borrow.route.js
@@ -1,13 +1,14 @@
 // mengimport variabel dari file lain dan dependensi yang dibutuhkan
 const borrowController = require('../controller/borrow.controller')
+const { authorize } = require('../middleware/auth')
 const express = require('express')
 const app = express()
 app.use(express.json())
 //menggunakan endpoint
-app.post("/", borrowController.addBorrowing)
-app.put("/:id", borrowController.updateBorrowing)
-app.delete("/:id", borrowController.deleteBorrowing)
-app.get("/return/:id", borrowController.returnBook)
-app.get("/", borrowController.getBorrow )
+app.post("/", [authorize],  borrowController.addBorrowing)
+app.put("/:id", [authorize], borrowController.updateBorrowing)
+app.delete("/:id", [authorize], borrowController.deleteBorrowing)
+app.get("/return/:id", [authorize], borrowController.returnBook)
+app.get("/", [authorize], borrowController.getBorrow )
 //mengeksport variabel supaya bisa digunakan di file lain
 module.exports = app
