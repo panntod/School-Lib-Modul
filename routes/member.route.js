69f5950bf2f430c1174ea6d9b661f0a39ea73e48
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-08 04:42:00 +0700

    [doc] added middleware validate

--- routes/member.route.js
+++ routes/member.route.js
@@ -2,13 +2,14 @@
 const express= require('express')
 const app = express()
 app.use(express.json())
+const { validateMember } = require('../middleware/validate')
 const { authorize } = require('../middleware/auth')
 const memberController = require('../controller/member.controller')
 // menggunakan enpoint
 app.get('/', [authorize], memberController.getAllMember)
-app.post('/', memberController.addMember)
+app.post('/', [validateMember], memberController.addMember)
 app.post('/find', [authorize], memberController.findMember)
-app.put('/:id', [authorize], memberController.updateMember)
+app.put('/:id', [validateMember], [authorize], memberController.updateMember)
 app.delete('/:id', [authorize], memberController.deleteMember)
 //mengeksport variabel supaya bisa digunakan di file lain
 module.exports = app
