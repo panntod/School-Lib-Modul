28bbb049ca25b9013191a09f110418ee94a6a34d
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-13 09:49:01 +0700

    [refactor] un used validate

--- routes/book.route.js
+++ routes/book.route.js
@@ -2,14 +2,13 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-const { validateBook } = require("../middleware/validate");
 const { authorize } = require("../middleware/auth");
 const bookController = require("../controller/book.controller");
 // menggunakan endpoint
 app.get("/", [authorize], bookController.getAllBook);
 app.post("/find", [authorize], bookController.findBook);
-app.post("/", [validateBook], [authorize], bookController.addBook);
-app.put("/:id", [validateBook], [authorize], bookController.updateBook);
+app.post("/", [authorize], bookController.addBook);
+app.put("/:id", [authorize], bookController.updateBook);
 app.delete("/:id", [authorize], bookController.deleteBook);
 //mengeksport variabel supaya bisa digunakan di file lain
 module.exports = app;
