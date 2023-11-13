45e38836d78e68bf67bf71aac7b8ce0c817ae53b
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-13 09:49:32 +0700

    [refactor] un used validate

--- routes/member.route.js
+++ routes/member.route.js
@@ -7,9 +7,9 @@ const { authorize } = require('../middleware/auth')
 const memberController = require('../controller/member.controller')
 // menggunakan enpoint
 app.get('/', [authorize], memberController.getAllMember)
-app.post('/', [validateMember], memberController.addMember)
+app.post('/', memberController.addMember)
 app.post('/find', [authorize], memberController.findMember)
-app.put('/:id', [validateMember], [authorize], memberController.updateMember)
+app.put('/:id', [authorize], memberController.updateMember)
 app.delete('/:id', [authorize], memberController.deleteMember)
 //mengeksport variabel supaya bisa digunakan di file lain
 module.exports = app
